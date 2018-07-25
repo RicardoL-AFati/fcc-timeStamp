390a3db4a1acd8ae02e8667a47ba3f384abd4da5
RicardoL-AFati <rickydem24@gmail.com>
2018-07-24 19:56:42 -0600

    added timestamp route

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  console.log(req);
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
