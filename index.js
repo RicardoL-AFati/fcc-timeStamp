1528afba221d5d473b437bb5b60bbec7aaa9bfaf
RicardoL-AFati <rickydem24@gmail.com>
2018-07-25 14:09:13 -0600

    attempted to add testing

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.json());
 
 app.get('/api/timestamp/', (req, res) => {
   res.send({ unix: new Date().getTime(), utc: new Date().toUTCString() });
@@ -11,13 +13,10 @@ app.get('/api/timestamp/', (req, res) => {
 
 app.get('/api/timestamp/:date_string?', (req, res) => {
   let date = req.params.date_string;
-  console.log(typeof date);
-  if (!date.match(/\d\d\d\d-\d\d-\d\d/)) {
+  if (!date.match(/\d{4}-\d\d-\d\d/)) {
     date = parseInt(date, 10);
   }
-  console.log(typeof date);
   const now = new Date(date);
-  console.log(now);
   if (now === 'Invalid Date') {
     res.send({ unix: 'null', utc: 'Invalid Date' });
   } else {
@@ -33,3 +32,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+module.exports = app;
